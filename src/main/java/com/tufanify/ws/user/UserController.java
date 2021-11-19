8aec2927888f09a026d23fd76643f192e75cb3ba
sakir tufan <sakir.tufan@familienservice.de>
2021-11-19 19:35:59 +0100

    Refactor User Controller

--- src/main/java/com/tufanify/ws/user/UserController.java
+++ src/main/java/com/tufanify/ws/user/UserController.java
@@ -1,14 +1,13 @@
 package com.tufanify.ws.user;
 
 
+import com.tufanify.ws.shared.GenericResponse;
 import lombok.extern.flogger.Flogger;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 public class UserController {
@@ -16,11 +15,13 @@ public class UserController {
 //    private static final Logger log = LoggerFactory.getLogger(UserController.class);
 
     @Autowired
-    UserRepository userRepository;
+    UserService userService;
 
 
     @PostMapping("/api/1.0/users")
-    public void createUser(@RequestBody User user){
-        userRepository.save(user);
+    @ResponseStatus(HttpStatus.CREATED)
+    public GenericResponse createUser(@RequestBody User user){
+        userService.save(user);
+        return new GenericResponse("User Created");
     }
 }
