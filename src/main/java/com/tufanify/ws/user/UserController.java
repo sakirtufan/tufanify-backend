01f16f2d6d2eecc6486b882b64c388749afa4c68
sakir tufan <sakir.tufan@familienservice.de>
2021-11-27 12:23:58 +0100

    Spring Validation

--- src/main/java/com/tufanify/ws/user/UserController.java
+++ src/main/java/com/tufanify/ws/user/UserController.java
@@ -2,12 +2,19 @@ package com.tufanify.ws.user;
 
 
 import com.tufanify.ws.shared.GenericResponse;
+import error.ApiError;
 import lombok.extern.flogger.Flogger;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.context.properties.bind.validation.ValidationErrors;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Validation;
+import java.util.HashMap;
+import java.util.Map;
 
 @RestController
 public class UserController {
@@ -20,8 +27,16 @@ public class UserController {
 
     @PostMapping("/api/1.0/users")
     @ResponseStatus(HttpStatus.CREATED)
-    public GenericResponse createUser(@RequestBody User user){
+    public ResponseEntity<?> createUser(@RequestBody User user){
+        String username = user.getUsername();
+        if(username == null || username.isEmpty()){
+            ApiError error = new ApiError(400,"Validation error", "/api/1.0/users");
+            Map<String, String> validationErrors = new HashMap<>();
+            validationErrors.put("username","Username cannot be null");
+            error.setValidationErrors(validationErrors);
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(error);
+        }
         userService.save(user);
-        return new GenericResponse("User Created");
+        return ResponseEntity.ok(new GenericResponse("User Created"));
     }
 }
